b35cbd08fac6e92e2f69cc12b416b7da57393aaf
kaigikinyua <kaigikinyua@gmail.com>
2024-10-29 14:06:44 +0300

    adding layout of the database class

--- MyShop/database.py
+++ MyShop/database.py
@@ -2,7 +2,24 @@ class DbUser:
     @staticmethod
     def login(username,password):
         pass
-    
+
     @staticmethod
     def logout(userToken):
         pass
+
+    @staticmethod
+    def isAuthenticated(userToken):
+        pass
+
+class DbSale:
+    pass
+
+class DbDelivery:
+    pass
+
+class DbStock:
+    pass
+
+class DbReports:
+    pass
+
