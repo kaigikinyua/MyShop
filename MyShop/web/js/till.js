3dad8ce9cd8413f9f6d35f444877f16836cc848a
kaigikinyua <kaigikinyua@gmail.com>
2024-10-29 19:07:29 +0300

    adding and removing items from the till ui done

--- MyShop/web/js/till.js
+++ MyShop/web/js/till.js
@@ -1,23 +1,52 @@
 var items=[]
-
+var total=0
 
 function addItem(){
     itemName=document.getElementById('itemCode').value
     quantity=document.getElementById('quantity').value
     price=100
     total=price*quantity
-    items.push({'name':itemName,'price':200,'quantity':quantity,'total':total})
-    var itemParent=document.getElementById('itemList')
-    var itemDiv=document.createElement('div')
-    itemDiv.classList.add('item')
-    itemDiv.id='item'+items.length
-    itemDiv.innerHTML=itemName+' @200 *'+quantity+' ='+total
-    itemParent.appendChild(itemDiv)
+    items.push({'name':itemName,'price':100,'quantity':quantity,'total':total})
+    renderItems(items)
+    computeTotal()
 
 }
+function renderItems(items){
+    var list=document.getElementById('itemList')
+    while(list.hasChildNodes()){
+        list.removeChild(list.firstChild)
+    }
+    var i=0
+    items.forEach(item=>{
+        i+=1
+        var itemParent=document.getElementById('itemList')
+        var itemDiv=document.createElement('div')
+        itemDiv.classList.add('item')
+        itemDiv.id='item,'+i
+        itemDiv.innerHTML=item['name']+' @'+item['price']+' '+item['quantity']+' ='+item['total']
+        var deleteItem=document.createElement('button')
+        deleteItem.addEventListener('click',()=>{removeItem(itemDiv.id)})
+        deleteItem.innerHTML="delete"
+        itemDiv.appendChild(deleteItem)
+        itemParent.appendChild(itemDiv)
+    });
+}
+
 function removeItem(itemId){
-    items.pop(itemId)
+    console.log("remove item"+itemId)
+    var id=itemId.split(',')
+    if(items.length>0){
+        items.splice(id,1)
+    }
+    renderItems(items)
 }
 
-function computeTotal(){}
+function computeTotal(){
+    var tot=0
+    items.forEach(i=>{
+        tot+=i["total"]
+    });
+    total=tot
+    document.getElementById("basketTotal").innerHTML=total
+}
 
