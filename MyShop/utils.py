1f8a0f3a0299df6b53b38de990bc4245e1601d8a
kaigikinyua <kaigikinyua@gmail.com>
2024-11-21 18:58:46 +0300

    adding fileUtils and Json

--- MyShop/utils.py
+++ MyShop/utils.py
@@ -1,4 +1,5 @@
-import datetime
+import datetime,os
+
 class FormatTime:
     @staticmethod
     def now():
@@ -13,9 +14,69 @@ class FormatTime:
     @staticmethod
     def getDateTime(timestamp):
         return datetime.datetime.fromtimestamp(timestamp)
+    
+    @staticmethod
+    def nowStandardTime():
+        date=datetime.datetime.now()
+        year=date.year
+        month=date.month
+        day=date.day
+        hour=date.hour
+        minute=date.minute
+        seconds=date.second
+        return f'{day}/{month}/{year} {hour}:{minute}:{seconds}'
+        
 
 class Logging:
-    pass
+    @staticmethod
+    def consoleLog(color,message):
+        colors={'err':'','succ':'','warn':'','norm':''}
+        print(message)
 
+    @staticmethod
+    def logToFile(type,message):
+        time=FormatTime.nowStandardTime()
+        line=f'{type}| {time}| {message}\n'
+        File.writeToFile()
+class JsonFile:
+    @staticmethod
+    def readJsonFile(filePath):
+        pass
+
+class File:
+    @staticmethod
+    def fileExists(filePath):
+        if(os.path.isfile(filePath)):
+            return True
+        return False
+
+    @staticmethod
+    def writeToFile(filePath,data):
+        if(File.fileExists(filePath)):
+            with open(filePath,'w') as f:
+                f.write(data)
+                f.close()
+                return True
+        else:
+            return False
+    @staticmethod
+    def createFile(filePath):
+        if File.fileExists(filePath):
+            return False
+        else:
+            f=open(filePath,'w')
+            f.close()
+            return True
+
+    @staticmethod
+    def readFromFile(filePath):
+        if(File.fileExists(filePath)):
+            f=open(filePath,'r')
+            data=f.readlines()
+            f.close()
+            return True,data
+        else:
+            return False,'File does not exist'
+        
 class CSV:
     pass
