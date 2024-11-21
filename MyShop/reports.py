878775c9f5e6afb0e4c55ec197472a392bdd350a
kaigikinyua <kaigikinyua@gmail.com>
2024-11-21 14:52:02 +0300

    first draft of the reports.py

---
+++ MyShop/reports.py
@@ -0,0 +1,12 @@
+from views import UserView,TransactionView,PaymentView,SoldItemsView
+class Reports:
+    #daily reports
+    def generateXReport():
+        pass
+    def generateZReport():
+        pass
+    def generateStockReport():
+        pass
+    def generateBusinessReport():
+        pass
+    
