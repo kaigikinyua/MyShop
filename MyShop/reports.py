a600bb6e4a7c5f7c7a1f61533db4bab912d5d5a4
kaigikinyua <kaigikinyua@gmail.com>
2024-12-06 23:34:55 +0300

    implementing some methods for generating csv reports in reports.py and writing some tests

--- MyShop/reports.py
+++ MyShop/reports.py
@@ -1,12 +1,57 @@
 from views import UserView,TransactionView,PaymentView,SoldItemsView
 class Reports:
     #daily reports
-    def generateXReport():
+    def generateXReport(self,dateTimeStamp):
         pass
-    def generateZReport():
+
+    def generateZReport(self,dateTimeStamp):
         pass
-    def generateStockReport():
+
+    def generateStockReport(self,startDate,endDate):
         pass
-    def generateBusinessReport():
+
+    def generateBusinessReport(self,startDate,endDate):
         pass
-    
+
+class CSV:
+    @staticmethod
+    def headers(headers,emptyStart):
+        if(len(headers)>0):
+            h=''
+            if(emptyStart==True):
+                h+=','
+            for header in headers:
+                h+=f'{str(header)},'
+            return f'{h}\n'
+        return False
+
+    @staticmethod
+    def rowData(two_D_array,numberedRow=False,emptyStart=True):
+        if(len(two_D_array)>0):
+            numRow=1
+            d=''
+            for row in two_D_array:
+                if(emptyStart):
+                    d=','
+                if(numberedRow):
+                    d=d+f'{str(numRow)},'
+                for elm in row:
+                    d=d+f'{elm},'
+                d=d+'\n'
+                numRow+=1
+            return d
+        
+    @staticmethod
+    def columData(header,data):
+        dataString=f'{str(header)},'
+        for d in data:
+            dataString+=f'{str(data)},\n'
+        return dataString
+
+        
+    @staticmethod
+    def dataSheet(headers,data):
+        headersString=CSV.headers(headers,True)
+        dataString=CSV.rowData(data,True,False)
+        dataSheet=headersString+dataString
+        return dataSheet
---
+++ MyShop/tests_reports.py
@@ -0,0 +1,33 @@
+import sys,unittest
+from unittest import TestCase
+
+
+from reports import CSV
+
+class TestReports(unittest.TestCase):
+    def test_headers(self):
+        result1=',Item Name,Bar Code,Quantity,\n'
+        array=['Item Name','Bar Code','Quantity']
+        csvHeader1=CSV.headers(array,True)
+        TestCase.assertEqual(self,result1,csvHeader1)
+        result2='Item Name,Bar Code,Quantity,\n'
+        csvHeader2=CSV.headers(array,False)
+        TestCase.assertEqual(self,result2,csvHeader2)
+
+    def test_rowData(self):
+        result1='1,100,200,400,500,600,\n2,123,456,789,345,301,\n'
+        array=[[100,200,400,500,600],[123,456,789,345,301]]
+        csvData1=CSV.rowData(array,numberedRow=True,emptyStart=False)
+        TestCase.assertEqual(self,result1,csvData1)
+
+
+    def test_dataSheet(self):
+        header=['Item Name','Bar Code','Quantity']
+        data=[[100,200,400,500,600],[123,456,789,345,301]]
+        dataString=CSV.dataSheet(header,data)
+        f=open('test_csv.csv','w')
+        f.write(dataString)
+        f.close()
+
+if __name__=='__main__':
+    unittest.main()
