a6bff18f875e39c80da2f25f8cc0b7e9d7082d79
kaigikinyua <kaigikinyua@gmail.com>
2024-12-09 23:50:00 +0300

    adding more settings variables to help in debugging and also setp

--- MyShop/settings.py
+++ MyShop/settings.py
@@ -1,3 +1,20 @@
+import datetime,json
 class Settings:
     dataBaseUrl="sqlite:///data/databases/current/myshop.db"
-    sessionLogDir="./data/logs/sessionLogs/"
+    configFileUrl="./data/configs/config.json"
+    mode="DEBUG" #[DEBUG|PROD]
+    
+    @staticmethod
+    def logFile():
+        sessionLogDir=f"./data/logs/sessionLogs/"
+        dTimeObj=datetime.datetime.now()
+        return f'{sessionLogDir}logFile_{dTimeObj.year}|{dTimeObj.month}|{dTimeObj.date}_at{dTimeObj.hour}|{dTimeObj.minute}.txt'
+    
+    @staticmethod
+    def tillId():
+        """f=open(Settings.configFileUrl,'r')
+        data=f.readlines()
+        f.close()
+        jData=json.loads(str(data))
+        return jData["tillId"]"""
+        return "MainStore"
