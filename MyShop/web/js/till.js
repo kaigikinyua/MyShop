d347e8068cb7c761443109aa23d17a986b339858
kaigikinyua <kaigikinyua@gmail.com>
2024-12-10 11:46:44 +0300

    storing the user shift id in local storage and fixing the notification bubble issue where the animation duration is not connected to the javascript also added the popup screen for the declare starting amount and declare closing amount

--- MyShop/web/js/till.js
+++ MyShop/web/js/till.js
@@ -2,9 +2,6 @@ var items=[]
 var productsList=[]
 var total=0
 
-function setUpUser(){
-    Auth.renderShiftId()
-}
 //user action buttons
 function addItem(){
     itemName=document.getElementById('itemCode').value
@@ -20,14 +17,14 @@ function addItem(){
                 Transaction.computeTotal()
                 itemFound=true
             }
-        })
+        });
         if(itemFound==false){
             console.log("There is no product with id "+itemName)
-            notificationBubble("There is no product with id "+itemName)
+            notificationBubble("There is no product with id "+itemName,2,4)
         }
     }
     else{
-        notificationBubble("Please fill in the item code and quantity",3)
+        notificationBubble("Please fill in the item code and quantity",2,4)
     }
 
 }
@@ -41,9 +38,89 @@ function removeItem(itemId){
     Render.renderItems(items)
 }
 
+
+//shift action buttons
+function displayStartingAmount(){
+    showPopUp("")
+    var popUpPanel=document.getElementById('popUpPanel')
+    
+    var header=document.createElement("h3")
+    header.classList.add("header")
+    header.innerHTML="Declare Starting Amount"
+    var sAmount=document.createElement("input")
+    sAmount.placeholder='Starting Amount'
+    sAmount.id="sAmountDeclaration"
+    var submit=document.createElement("button")
+    submit.innerHTML="Submit"
+    //submit.classList.add("submit")
+    submit.classList.add("minLenBtn")
+    submit.classList.add("danger")
+    submit.onclick=(()=>{
+        var sAmount=document.getElementById("sAmountDeclaration").value
+        if(sAmount!=undefined && sAmount!=null && sAmount!=""){
+            Shift.declareStartingAmount(sAmount)
+            closePopUp()
+            notificationBubble("Declared starting amount",1,3)
+        }else{
+            notificationBubble("Please fill in the starting amount",2,3)
+        }
+    })
+    var cancel=document.createElement("button")
+    cancel.innerHTML="Cancel"
+    cancel.classList.add("minLenBtn")
+    cancel.classList.add("cool")
+    cancel.onclick=closePopUp
+
+    popUpPanel.appendChild(header)
+    popUpPanel.appendChild(sAmount)
+    popUpPanel.appendChild(submit)
+    popUpPanel.appendChild(cancel)
+}
+function displayClosingAmount(){
+    showPopUp("")
+    var popUpPanel=document.getElementById('popUpPanel')
+    
+    var header=document.createElement("h3")
+    header.classList.add("header")
+    header.innerHTML="Declare Closing Amount"
+    var cAmount=document.createElement("input")
+    cAmount.placeholder='Closing Amount'
+    cAmount.id="cAmountDeclaration"
+    var submit=document.createElement("button")
+    submit.innerHTML="Submit"
+    //submit.classList.add("submit")
+    submit.classList.add("minLenBtn")
+    submit.classList.add("danger")
+    submit.onclick=(()=>{
+        var cAmount=document.getElementById("cAmountDeclaration").value
+        if(cAmount!=undefined && cAmount!=null && cAmount!=""){
+            Shift.declareClosingAmount(sAmount)
+            closePopUp()
+            notificationBubble("Declared closing amount",1,3)
+        }else{
+            notificationBubble("Please fill in the closing amount",2,3)
+        }
+    })
+    var cancel=document.createElement("button")
+    cancel.innerHTML="Cancel"
+    cancel.classList.add("minLenBtn")
+    cancel.classList.add("cool")
+    cancel.onclick=closePopUp
+
+    popUpPanel.appendChild(header)
+    popUpPanel.appendChild(cAmount)
+    popUpPanel.appendChild(submit)
+    popUpPanel.appendChild(cancel)
+}
+
 async function payment(){
     //var x=await eel.makeSale()
 }
+
+function setUpUser(){
+    Auth.renderShiftId()
+}
+
 
 class Render{
     static renderItems(items){
@@ -87,6 +164,12 @@ class FetchData{
     }
 }
 class Shift{
+    static declareStartingAmount(amount){
+        console.log("Starting amount is "+amount)
+    }
+    static declareClosingAmount(amount){
+        console.log("Closing amount is "+amount)
+    }
     openShift(){}
     closeShift(){}
 }
